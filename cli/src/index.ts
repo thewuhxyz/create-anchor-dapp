865fbb2caf02cf69bb6beb353cad2bf433a2a629
chizor iwuh <thisischizor@gmail.com>
2024-06-23 14:22:47 +0100

    configure package manager

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -3,41 +3,50 @@ import { program } from "@commander-js/extra-typings"
 import fs from "fs-extra"
 import path from "path"
 import { fileURLToPath } from "url"
-import { getUserPkgManager } from "./helpers"
+import { addName, addScripts, addWorkspace, getUserPkgManager } from "./utils"
 
 const __filename = fileURLToPath(import.meta.url)
 console.log("fileName:", __filename)
+
 const binPath = path.dirname(__filename)
 console.log("bin path:", binPath)
+
 const distPath = path.join(binPath, "../../")
 console.log("dist path:", distPath)
-
 
 program
 	.name("create-anchor-app")
 	.description("Scaffold an Solana project with Anchor")
 	.version("0.0.1")
 
-program.action(() => {
+program.argument("<name>").action((name) => {
 	console.log("creating anchor app.....")
+
+	const projectDir = path.resolve(process.cwd(), name)
+	const baseDir = path.join(distPath, "template/base")
+	const addonsDir = path.join(distPath, "template/add-ons")
+
+	const pkg = getUserPkgManager()
+	console.log("package manager:", pkg)
+
+	console.log("project dir:", projectDir)
+	console.log("src dir:", baseDir)
+
+	fs.copySync(baseDir, projectDir)
+
+	if (pkg === "pnpm") {
+		const srcPath = path.join(addonsDir, "package-manager/pnpm-workspace.yaml")
+		const destPath = path.join(projectDir, "pnpm-workspace.yaml")
+		fs.copyFileSync(srcPath, destPath)
+	} else {
+		addWorkspace({ projectDir })
+	}
+
+	addScripts({ projectDir, pkg })
+
+	addName({ projectDir, name })
+
 	console.log("created anchor app")
 })
-
-program
-	.argument("<name>")
-	.action((name) => { 
-		console.log("creating anchor app.....")
-		console.log("creating app at ", name, "...")
-		const projectDir = path.resolve(process.cwd(), name)
-		const srcDir = path.join(distPath, "template/base")
-
-		const pkg = getUserPkgManager()
-		console.log("package manager:", pkg)
-
-		console.log("project dir:", projectDir)
-		console.log("src dir:", srcDir)
-		fs.copySync(srcDir, projectDir)
-		console.log("created anchor app")
-	})
 
 program.parse()
---
+++ cli/src/utils/index.ts
@@ -0,0 +1,1 @@
+export * from "./configure-package-manager"
